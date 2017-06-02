4e76510d6fcf5f6a005e09666a0b247c3317a3ba
Dave Brown <davypough@gmail.com>
2017-06-02 14:59:34 -0700

    Add files via upload

--- src/installer.lisp
+++ src/installer.lisp
@@ -3,14 +3,14 @@
 ;;; Installs a user domain file.
 
 
-(in-package :pl)
+(in-package :ww)
 
 
 (defmacro define-types (&rest types&objects)
-  `(install_types ',types&objects))
+  `(install-types ',types&objects))
 
 
-(defun install_types (types&objects)
+(defun install-types (types&objects)
   (declare (special *db* *types*))
   (format t "~%Installing object types...~%")
   (loop for (type objects) on types&objects by #'cddr
@@ -27,15 +27,15 @@
 
 
 (defmacro define-base-relations (&rest relations)
-  `(install_base_relations ',relations))
+  `(install-base-relations ',relations))
 
 
-(defun install_base_relations (relations)
+(defun install-base-relations (relations)
   (declare (hash-table *types* *relations*) (special *types* *relations* *symmetrics*))
   (format t "Installing base relations...~%")
   (loop for relation in relations
       do (setf (gethash (car relation) *relations*) 
-           (ut::if-it (cdr relation) ut::it t))
+           (ut::if-it (cdr relation) ut::it nil))
       finally (maphash #'(lambda (key value)  ;install implied unary relations
                            (declare (ignore value))
                            (setf (gethash key *relations*) '(everything)))
@@ -43,26 +43,26 @@
   (loop for key being the hash-keys of *relations*  ;install symmetric relations
         using (hash-value value)
       when (not (alexandria:setp value))  ;multiple types
-        do (setf (gethash key *symmetrics*) (symmetric_type_indexes value)))
+        do (setf (gethash key *symmetrics*) (symmetric-type-indexes value)))
   *relations*)
 
 
 (defmacro define-monitored-relations (&rest relations)
-  `(install_monitored_relations ',relations))
+  `(install-monitored-relations ',relations))
 
 
-(defun install_monitored_relations (relations)
-  (declare (special *monitored_relations*))
+(defun install-monitored-relations (relations)
+  (declare (special *monitored-relations*))
   (format t "Installing monitored relations...~%")
   (loop for relation in relations
-        do (setf (gethash relation *monitored_relations*) t)))
+        do (setf (gethash relation *monitored-relations*) t)))
 
 
 (defmacro define-derived-relations (&rest conditions&bodies)
-  `(install_derived_relations ',conditions&bodies))
+  `(install-derived-relations ',conditions&bodies))
 
 
-(defun install_derived_relations (conditions&bodies)
+(defun install-derived-relations (conditions&bodies)
   (declare (special *derived*))
   (format t "Installing derived relations...~%")
   (loop for (condition body) on conditions&bodies by #'cddr
@@ -75,10 +75,10 @@
         
 
 (defmacro define-happening (object &rest plist)
-  `(install_happening ',object ',plist))
+  `(install-happening ',object ',plist))
 
 
-(defun install_happening (object plist)
+(defun install-happening (object plist)
   (declare (special *happenings*))
   (format t "Installing happening for ~A~%" object)
   (setf (symbol-plist object) plist)  ;overwrite any settings from a prior problem
@@ -86,20 +86,20 @@
   (when (get object :interrupt)
     (setf (get object :interrupt) `(lambda (state)
                                      ,(translate (get object :interrupt) 'pre)))
-    (setf (the function (get object :interrupt_fn)) (compile nil (get object :interrupt))))
+    (setf (the function (get object :interrupt-fn)) (compile nil (get object :interrupt))))
   (when (get object :rebound) 
     (setf (get object :rebound) `(lambda (state)
                                      ,(translate (get object :rebound) 'pre)))
-    (setf (the function (get object :rebound_fn)) (compile nil (get object :rebound))))
+    (setf (the function (get object :rebound-fn)) (compile nil (get object :rebound))))
   (push object *happenings*))
            
 
 (defmacro define-function (name args body)
-  `(install_function ',name ',args ',body))
+  `(install-function ',name ',args ',body))
 
 
-(defun install_function (name args body)
-  (declare (special *function_names*))
+(defun install-function (name args body)
+  (declare (special *function-names*))
   (format t "Installing ~A function...~%" name)
   (setf (symbol-value name)
     (let* ((fbody (alexandria:flatten body))
@@ -111,58 +111,58 @@
            (let ,(map 'list #'(lambda ($var) (list $var 0)) sbody)
              (declare (special ,@sbody))
              ,(translate body 'pre)
-             (list ,@(get_vars #\$ args)))))))  ;to prevent compiler warning
+             (list ,@(get-vars #\$ args)))))))  ;to prevent compiler warning
   (eval `(defun ,name ,@(cdr (symbol-value name))))
-  (push name *function_names*))
+  (push name *function-names*))
   
 
 (defmacro define-constraint (form)
-  `(install_constraint ',form))
+  `(install-constraint ',form))
 
 
-(defun install_constraint (form)
+(defun install-constraint (form)
   (declare (special *constraint*))
   (format t "Installing constraint...~%")
   (setq *constraint* `(lambda (state) ,(translate form 'pre))))
         
 
-(defmacro define-action (name duration pre_params precondition eff_params effect)
-  `(install_action ',name ,duration ',pre_params ',precondition ',eff_params ',effect))
+(defmacro define-action (name duration pre-params precondition eff-params effect)
+  `(install-action ',name ,duration ',pre-params ',precondition ',eff-params ',effect))
 
 
-(defun install_action (name duration pre_params precondition eff_params effect)
+(defun install-action (name duration pre-params precondition eff-params effect)
   (declare (special *actions*))
   (format t "Installing ~A action...~%" name)
-  (destructuring-bind (pre_vars pre_types) (ut::segregate-plist (expand_into_plist pre_params))
-    (let ((pre_vars? (get_vars #\? pre_vars))
-          (pre_vars$ (get_vars #\$ pre_vars))
-          (pre_types (delete 'fluent pre_types)))
-      (destructuring-bind (eff_vars eff_types) (ut::segregate-plist (expand_into_plist eff_params))
-        (let ((eff_vars? (get_vars #\? eff_vars))
-              (eff_vars$ (get_vars #\$ eff_vars))
-              (eff_types (delete 'fluent eff_types)))
+  (destructuring-bind (pre-vars pre-types) (ut::segregate-plist (expand-into-plist pre-params))
+    (let ((pre-vars? (get-vars #\? pre-vars))
+          (pre-vars$ (get-vars #\$ pre-vars))
+          (pre-types (delete 'fluent pre-types)))
+      (destructuring-bind (eff-vars eff-types) (ut::segregate-plist (expand-into-plist eff-params))
+        (let ((eff-vars? (get-vars #\? eff-vars))
+              (eff-vars$ (get-vars #\$ eff-vars))
+              (eff-types (delete 'fluent eff-types)))
           (let ((action (make-action
                          :name name
                          :duration duration
-                         :precondition_variables pre_vars?
-                         :precondition_types pre_types
-                         :precondition_lambda `(lambda ,(cons 'state pre_vars?)
+                         :precondition-variables pre-vars?
+                         :precondition-types pre-types
+                         :precondition-lambda `(lambda ,(cons 'state pre-vars?)
                                                  (let ,(map 'list #'(lambda ($var)
                                                                        (list $var 0))
-                                                           pre_vars$)
+                                                           pre-vars$)
                                                    ;special allows setting of local $var
-                                                   (declare (special ,@pre_vars$))
+                                                   (declare (special ,@pre-vars$))
                                                    (when ,(translate precondition 'pre)
                                                      ;return fluent values or satisfied pre
-                                                     (or (list ,@eff_vars$) t))))
-                         :effect_variables eff_vars?
-                         :effect_types eff_types
-                         :effect_lambda `(lambda (state ,@eff_vars? ,@eff_vars$)
+                                                     (or (list ,@eff-vars$) t))))
+                         :effect-variables eff-vars?
+                         :effect-types eff-types
+                         :effect-lambda `(lambda (state ,@eff-vars? ,@eff-vars$)
                                            (let (changes)
                                              ,(translate effect 'eff)
                                              changes)))))
-            (dolist (form (list (action-precondition_lambda action) 
-                                (action-effect_lambda action)))
+            (dolist (form (list (action-precondition-lambda action) 
+                                (action-effect-lambda action)))
               (when (not (ut::walk-tree-until (lambda (x)
                                                 (eq x 'state))
                                               (third form))) ;db in the lambda body?
@@ -172,23 +172,23 @@
 
 
 (defmacro define-init (&rest propositions)
-  `(install_init ',propositions))
+  `(install-init ',propositions))
 
 
-(defun install_init (propositions)
+(defun install-init (propositions)
   (declare (hash-table *db*) (special *db*))
   (format t "Installing initial propositional database...~%")
   (loop for proposition in propositions
-      do (add_proposition proposition *db*))
-  (add_proposition '(always-true) *db*)
+      do (add-proposition proposition *db*))
+  (add-proposition '(always-true) *db*)
   *db*)
 
 
 (defmacro define-goal (form)
-  `(install_goal ',form))
+  `(install-goal ',form))
 
 
-(defun install_goal (form)
+(defun install-goal (form)
   (declare (special *goal*))
   (format t "Installing goal...~%")
   (setq *goal* `(lambda (state) ,(translate form 'pre)))
