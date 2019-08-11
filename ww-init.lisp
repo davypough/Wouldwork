09eab6c60a41d929c75db17529fd76e3a2467853
Dave Brown <davypough@gmail.com>
2019-08-11 14:01:31 -0700

    Add files via upload

--- ww-init.lisp
+++ ww-init.lisp
@@ -1,46 +1,37 @@
 (setq *default-pathname-defaults*
         #p"D:\\Users Data\\Dave\\SW Library\\AI\\Planning\\Wouldwork Planner\\")
 
-
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp\\setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
-
 
 (asdf:initialize-source-registry
   `(:source-registry
      (:tree ,*default-pathname-defaults*)
      :inherit-configuration))
 
-
 (ql:quickload :alexandria)
-
 (ql:quickload :iterate)
-
 (ql:quickload :lparallel)
-
 
 #+:sbcl (setq sb-ext:*debug-print-variable-alist*
           '((*print-length* . 70) (*print-level* . 6) (*print-pretty* . t)))
-(setq *compile-verbose* nil)
 
+(setq *compile-verbose* nil)
 
 (defpackage :utilities-pkg
   (:use :cl)
   (:nicknames :ut))
 
-
 (defpackage :hstack-pkg
   (:use :cl)
   (:nicknames :hs))
-
 
 (defpackage :wouldwork-pkg
   (:use :cl :iterate)
   (:shadowing-import-from :iterate :sum)
   (:nicknames :ww))
-
 
 ;;;;;;;;;;;;;; Testing ;;;;;;;;;;;;;;;;;
 
@@ -49,7 +40,7 @@
 (defparameter *problem-files*
   '("problem-blocks.lisp" "problem-boxes.lisp" "problem-2jugs.lisp" "problem-4jugs.lisp"
     "problem-4queens.lisp" "problem-8queens.lisp" "problem-captjohn.lisp" "problem-crater.lisp"
-    "problem-hanoi.lisp" "problem-knap4.lisp" "problem-knap19.lisp" "problem-sentry.lisp" "problem-smallspace.lisp"
+    "problem-hanoi.lisp" "problem-knap4.lisp" "problem-knap19.lisp" "problem-sentry.lisp" ;"problem-smallspace.lisp"
     "problem-smallspace2.lisp" "problem-triangle.lisp" "problem-u2.lisp"))
 
 (declaim (ftype (function () t) solve))
