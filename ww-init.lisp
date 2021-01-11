2ecac1fec124e7dd615e5799042645c9ccadd9e2
Dave Brown <davypough@gmail.com>
2021-01-11 10:58:09 -0800

    Add files via upload

--- ww-init.lisp
+++ ww-init.lisp
@@ -4,33 +4,44 @@
 
 (in-package :cl-user)
 
+
 (setq *default-pathname-defaults*
-        #p"D:\\Users Data\\Dave\\SW Library\\AI\\Planning\\Wouldwork Planner\\")
+      #p"D:\\Users Data\\Dave\\SW Library\\AI\\Planning\\Wouldwork Planner\\")
+
 
 #-quicklisp
 (let ((quicklisp-init (merge-pathnames "quicklisp\\setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
-(asdf:initialize-source-registry
-  `(:source-registry
-     (:tree ,*default-pathname-defaults*)
-     :inherit-configuration))
+
+;(asdf:initialize-source-registry
+;  `(:source-registry
+;     (:tree "D:\\Users Data\\Dave\\SW Library\\AI\\")  ;,*default-pathname-defaults*)
+;     :inherit-configuration))
+
+
+(pushnew #p"D:\\Users Data\\Dave\\SW Library\\AI\\Planning\\Wouldwork Planner\\" asdf:*central-registry* :test #'equal)
+(pushnew #p"D:\\Users Data\\Dave\\SW Library\\AI\\Lisp\\Common Lisp Libraries\\Lisp Critic\\" asdf:*central-registry* :test #'equal)
+
 
 #+:sbcl
 (setq sb-ext:*debug-print-variable-alist*
   '((*print-length* . 70) (*print-level* . 6)))
-
 (setq *compile-verbose* nil)
 (setq *compile-print* nil)
+
 
 (ql:quickload :alexandria)
 (ql:quickload :iterate)
 (ql:quickload :lparallel)
 
+
 ;(asdf:load-system :lisp-critic)
 ;(defpackage :lisp-critic
 ;  (:nicknames :lc))
+;(use-package :lisp-critic)
+
 
 (defpackage :utilities-pkg
   (:use :cl)
@@ -54,6 +65,7 @@
 
 (in-package :ww)
 
+
 (defparameter *problem-files*
   '("problem-blocks3.lisp" "problem-blocks4.lisp" "problem-boxes.lisp" "problem-jugs2.lisp" "problem-jugs4.lisp"
     "problem-queens8.lisp" "problem-captjohn.lisp" "problem-crater.lisp" "problem-graveyard.lisp"
@@ -61,7 +73,9 @@
     "problem-triangle.lisp" "problem-triangle-xyz.lisp" "problem-triangle-heuristic.lisp"
     "problem-triangle-macros-one.lisp" "problem-tsp.lisp" "problem-u2.lisp"))
 
+
 (declaim (ftype (function () t) solve))
+
 
 (defun test ()
   (when (probe-file "problem.lisp")
@@ -72,4 +86,4 @@
              (with-open-stream (*standard-output* (make-broadcast-stream)) ;ignore *standard-output*
                (asdf:load-system :wouldwork-planner :force t))
              (solve)
-             (rename-file "problem.lisp" filename)))
+        (rename-file "problem.lisp" filename)))
