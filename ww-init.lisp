20d7741169d1377d5762f643c349b9e559cf7474
Dave Brown <davypough@gmail.com>
2023-05-05 12:24:50 -0700

    Add files via upload

--- ww-init.lisp
+++ ww-init.lisp
@@ -5,46 +5,36 @@
 (in-package :cl-user)
 
 
-(setq *default-pathname-defaults*
-      #p"D:\\Users Data\\Dave\\SW Library\\AI\\Planning\\Wouldwork Planner\\")
-
-
 #-quicklisp
-(let ((quicklisp-init (merge-pathnames "quicklisp\\setup.lisp" (user-homedir-pathname))))
+(let ((quicklisp-init (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
   (when (probe-file quicklisp-init)
     (load quicklisp-init)))
 
 
-;(asdf:initialize-source-registry
-;  `(:source-registry
-;     (:tree ,*default-pathname-defaults*)
-;     :inherit-configuration))
+(setq *default-pathname-defaults*
+      #p"D:/Users Data/Dave/SW Library/AI/Planning/Wouldwork Planner/")
 
 
-(pushnew #p"D:\\Users Data\\Dave\\SW Library\\AI\\Planning\\Wouldwork Planner\\"
-         asdf:*central-registry* :test #'equal)
+(asdf:initialize-source-registry
+  `(:source-registry
+     (:directory ,*default-pathname-defaults*)
+     (:directory #p"D:/Users Data/Dave/SW Library/AI/Lisp/Common Lisp Libraries/Lisp Critic/")
+     :inherit-configuration))
 
 
-;(pushnew #p"D:\\Users Data\\Dave\\SW Library\\AI\\Lisp\\Common Lisp Libraries\\Lisp Critic\\"
-;         asdf:*central-registry* :test #'equal)
+;#+:sbcl
+;(setq sb-ext:*debug-print-variable-alist*
+;  '((*print-length* . 70) (*print-level* . 6)))
 
-
-#+:sbcl
-(setq sb-ext:*debug-print-variable-alist*
-  '((*print-length* . 70) (*print-level* . 6)))
 (setq *compile-verbose* nil)
+
 (setq *compile-print* nil)
 
 
 (ql:quickload :alexandria)
 (ql:quickload :iterate)
 (ql:quickload :lparallel)
-
-
-;(asdf:load-system :lisp-critic)
-;(defpackage :lisp-critic
-;  (:nicknames :lc))
-;(use-package :lisp-critic)
+;(ql:quickload :cl-cuda)
 
 
 ;(require :sb-cltl2)  ;enable access to declarations in .asd file
@@ -59,23 +49,33 @@
   (:nicknames :hs))
 
 (defpackage :wouldwork-pkg
-  (:use :cl :iterate)
+  (:use :cl :iterate :sb-ext)
   (:shadowing-import-from :iterate)
   (:nicknames :ww))
 
 
+(setq sb-ext:*debug-print-variable-alist*
+      '((*print-length* . 30) (*print-level* . 6) (*print-pretty* . T)))
+
+
 ;;;;;;;;;;;;;; Testing ;;;;;;;;;;;;;;;;;
+
 
 (in-package :ww)
 
 
 (defparameter *problem-files*
-  '("problem-blocks3.lisp" "problem-blocks4.lisp" "problem-boxes.lisp" "problem-jugs2.lisp"
-    "problem-jugs4.lisp" "problem-queens8.lisp" "problem-captjohn.lisp" "problem-crater.lisp"
-    "problem-graveyard.lisp"
-    "problem-knap19.lisp" "problem-sentry.lisp" "problem-smallspace.lisp" "problem-smallspace2.lisp"
-    "problem-triangle-xy.lisp" "problem-triangle-xyz.lisp" "problem-triangle-heuristic.lisp"
-    "problem-triangle-macros-one.lisp" "problem-tsp.lisp" "problem-u2.lisp"))
+  '(
+    "problem-blocks3.lisp" "problem-blocks4.lisp" "problem-boxes.lisp"
+    "problem-jugs2.lisp" "problem-jugs4.lisp" "problem-queens4.lisp"
+    "problem-queens8.lisp" "problem-captjohn.lisp" "problem-quern.lisp" 
+    "problem-graveyard.lisp" "problem-sentry.lisp" "problem-crossword5-11.lisp"
+    "problem-triangle-xy.lisp"
+    "problem-triangle-xyz.lisp" "problem-triangle-heuristic.lisp"
+    "problem-triangle-macros.lisp" "problem-triangle-macros-one.lisp"
+    "problem-tsp.lisp" "problem-u2.lisp" "problem-donald.lisp"
+    "problem-knap4a.lisp" "problem-knap4b.lisp" "problem-knap19.lisp"
+    "problem-smallspace.lisp" "problem-crater.lisp"))
 
 
 (declaim (ftype (function () t) solve))  ;function solve located in searcher.lisp
@@ -85,9 +85,10 @@
   (when (probe-file "problem.lisp")
     (error "Rename problem.lisp before testing."))
   (loop for filename in *problem-files*
-        when (y-or-n-p "Proceed with testing ~A?:" filename)
-          do (rename-file filename "problem.lisp")
-             (with-open-stream (*standard-output* (make-broadcast-stream)) ;ignore *standard-output*
-               (asdf:load-system :wouldwork-planner :force t))
-             (solve)
-        (rename-file "problem.lisp" filename)))
+        if (y-or-n-p "Proceed with testing ~A?:" filename)
+         do (rename-file filename "problem.lisp")
+            (with-open-stream (*standard-output* (make-broadcast-stream)) ;ignore *standard-output*
+              (asdf:load-system :ww-wouldwork-planner :force t))
+            (solve)
+            (rename-file "problem.lisp" filename)
+         else do (return)))
