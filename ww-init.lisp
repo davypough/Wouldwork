cc20bbdaebc5f16d3b8c7814fb584388de3e6e71
Dave Brown <davypough@gmail.com>
2024-07-08 15:23:07 -0700

    Add files via upload

--- ww-init.lisp
+++ ww-init.lisp
@@ -12,8 +12,10 @@
 
 
 (setq *default-pathname-defaults*
-      #p"D:/Users Data/Dave/SW Library/AI/Planning/Wouldwork Planner/")
-
+     #p"D:/Users Data/Dave/SW Library/AI/Planning/Wouldwork Planner/")
+     ;#p"D:/Users Data/Dave/SW Library/AI/Planning/Wouldwork Planner/temp-21apr/")
+     ;#p"D:/Users Data/Dave/SW Library/AI/Planning/Wouldwork Planner/archive/200-add combinations/")
+      
 
 (asdf:initialize-source-registry
   `(:source-registry
@@ -34,6 +36,7 @@
 (ql:quickload :alexandria)
 (ql:quickload :iterate)
 (ql:quickload :lparallel)
+
 ;(ql:quickload :cl-cuda)
 
 
@@ -68,10 +71,10 @@
   '(
     "problem-blocks3.lisp" "problem-blocks4.lisp" "problem-boxes.lisp"
     "problem-jugs2.lisp" "problem-jugs4.lisp" "problem-queens4.lisp"
-    "problem-queens8.lisp" "problem-captjohn.lisp" "problem-quern.lisp" 
+    "problem-queens8.lisp" "problem-captjohn-csp.lisp" "problem-quern.lisp" 
     "problem-graveyard.lisp" "problem-sentry.lisp" "problem-crossword5-11.lisp"
-    "problem-array-path.lisp" "problem-triangle-xy.lisp"
-    "problem-triangle-xyz.lisp" "problem-triangle-heuristic.lisp"
+    "problem-array-path.lisp" "problem-tiles1a-heuristic.lisp" "problem-tiles7a-heuristic.lisp"
+    "problem-triangle-xy.lisp" "problem-triangle-xyz.lisp" "problem-triangle-heuristic.lisp"
     "problem-triangle-macros.lisp" "problem-triangle-macros-one.lisp"
     "problem-tsp.lisp" "problem-u2.lisp" "problem-donald.lisp"
     "problem-knap4a.lisp" "problem-knap4b.lisp" "problem-knap19.lisp"
@@ -85,10 +88,20 @@
   (when (probe-file "problem.lisp")
     (error "Rename problem.lisp before testing."))
   (loop for filename in *problem-files*
-        if (y-or-n-p "Proceed with testing ~A?:" filename)
-         do (rename-file filename "problem.lisp")
-            (with-open-stream (*standard-output* (make-broadcast-stream)) ;ignore *standard-output*
-              (asdf:load-system :ww-wouldwork-planner :force t))
-            (solve)
-            (rename-file "problem.lisp" filename)
-         else do (return)))
+        if (y-or-n-p "Proceed with testing ~A?: " filename)
+          do (rename-file filename "problem.lisp")
+             (handler-bind ((sb-sys:interactive-interrupt
+                             (lambda (c)
+                               (declare (ignore c))
+                               (throw 'my-loop nil))))
+               (catch 'my-loop
+                 (labels ((my-loop ()
+                            (with-open-stream (*standard-output* (make-broadcast-stream))
+                              (asdf:load-system :ww-wouldwork-planner :force t))
+                            (solve)))
+                   (my-loop))))
+             (rename-file "problem.lisp" filename)
+        else do (return)))
+
+
+(format t "~A~2%" *default-pathname-defaults*)
